aa62ffebd23188b8ac03fe36b612b93ae41b09a6
Harish Hardy <mj.harishardy@gmail.com>
2019-05-22 10:21:46 +0530

    firstCommit

---
+++ customTableView/TableViewCellX1.swift
@@ -0,0 +1,35 @@
+//
+//  TableViewCellX1.swift
+//  customTableView
+//
+//  Created by Harish Hardy on 22/05/19.
+//  Copyright © 2019 iosdev. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCellX1: UITableViewCell {
+
+   
+    
+    
+    
+    
+    @IBOutlet var labelX1 : UILabel!
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- customTableView/ViewController.swift
+++ customTableView/ViewController.swift
@@ -8,7 +8,25 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDataSource {
+   
+    var arrayX1 = ["China","Antartica","Europe","Africa","Pakistan","Australia","India","Korea","Singapore","Saudi Arabia","Malasyia","Denmark","Mexico","Canada","Iceland","Bulgaria","London","Iraq","France","Afganistan","Algeria","Sri Lanka","New Zealand","Maldives","Zambia","Italy","Belgium","Hungary","Bhutan","Libya","Egypt","Myanmar","Austria"]
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return arrayX1.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cellX1 = tableView.dequeueReusableCell(withIdentifier: "cellIdentifierX1", for: indexPath) as! TableViewCellX1
+        
+        cellX1.labelX1.text = arrayX1[indexPath.row]
+        
+        return cellX1
+    }
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
